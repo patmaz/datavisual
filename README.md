35af1b425a34814b25326b7349ad774e7fd73f53
patmaz <patrykiewicz@gmail.com>
2016-01-01 14:11:24 +0100

    js optimisation graphics fill between lines

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Data Visualisation
 The following technologies were used:
 
-* jquery - a basic framework :)
-* jqplot - a plotting and charting plugin
+* jquery - a basic framework required by jqplot...
+* jqplot - a plotting and charting plugin - quite efficient/flexible and relatively easy in use
 * some native js code for calculations on provided data
+
+more comments mainly in custom.js file
--- js/custom.js
+++ js/custom.js
@@ -1,89 +1,105 @@
+// ###################################### DATA
+// series for revenue line chart
+var revenue1 = [100, 200, 300, 400, 300]; //label:'NetComp'
+var revenue2 = [500, 600, 700, 800, 700]; //label:'AnalyzerHR'
+var revenue3 = [900, 1000, 1100, 1200, 1100]; //label:'QuestionRight"
+
+//series for installations bar chart
+var installations1 = [10, 10, 10, 10, 10]; // label:'NetComp'
+var installations2 = [20, 20, 20, 20, 20]; // label:'AnalyzerHR'
+var installations3 = [30, 30, 30, 30, 30]; // label:'Question Right'
+
+//labels
+var labels = ["NetComp", "AnalyzerHR", "QuestionRight"];
+
+//time points for both graphs (axis is categorical)
+var weeks = ["week 1", "week 2", "week 3", "week 4", "week 5"];
+
 $(document).ready(function(){
     
-    //line chart revenue
-    
-    // series as arrays nested in arrays
-    var line1 = [['week 50', 421], ['week 51', 560], ['week 52', 390], ['week 53', 500]]; //label:'NetComp'
-    var line2 = [['week 50', 500], ['week 51', 430], ['week 52', 200], ['week 53', 440]]; //label:'AnalyzerHR'
-    var line3 = [['week 50', 300], ['week 51', 250], ['week 52', 355], ['week 53', 120]]; //label:'Question Right'
-    
-    // line chart generator
-    var plot1 = $.jqplot('plot1', [line1, line2, line3], {
+    // #####################################
+    // #################line chart generator
+    var plot1 = $.jqplot('plot1', [revenue1, revenue2, revenue3], {
         //legend options
         legend: {
             renderer: $.jqplot.EnhancedLegendRenderer,
             show:true,
             placement: "outsideGrid",
-            location:'s',
+            location: "s",
             rendererOptions: {
-                numberRows: '1',
-                numberColumns: '3'
+                numberRows: "1",
             },
+        },
+        //fill between lines
+        fillBetween: {
+            series1: 0, //1st series
+            series2: 2, //2nd series
+            color: "#e7f6ff",
         },
         // background and whole grid
         grid: {
-            background: "white"
+            background: "white",
+            borderColor: "transparent", 
+            shadow: false
         },
         //axes options
         axes: {
             xaxis: {
               renderer: $.jqplot.CategoryAxisRenderer,
+                ticks: weeks, // weeks array
               label: '',
                 tickOptions:{
                     showGridline: false, //no vertical lines
-                    textColor: '#05415b'
+                    textColor: "#05415b"
                 }
             },
             yaxis: {
+                min: 0, //min value
                 tickOptions:{
-                    formatString:'$%d',
-                    textColor: '#05415b'
-                } // add $
+                    formatString:'$%d', // add $
+                    textColor: "#05415b"
+                } 
             }
       },
         //styles and lebales for series
         series:[ 
-          {lineWidth:5, markerOptions: { size: 10 }, color: "red", label:'NetComp'}, 
-          {lineWidth:5, markerOptions: { size: 10 }, color: "blue", label:'AnalyzerHR'},
-          {lineWidth:5, markerOptions: { size: 10 }, color: "green", label:'Question Right'}
+          {lineWidth:5, markerOptions: { size: 10 }, color: "#ed6e37", label: labels[0], shadow: false}, 
+          {lineWidth:5, markerOptions: { size: 10 }, color: "#15a0c8", label: labels[1], shadow: false},
+          {lineWidth:5, markerOptions: { size: 10 }, color: "#259e01", label: labels[2], shadow: false}
         ]
     });
     
     // /line chart
     
-    // bar chart installations
-    
-    //series as arrays
-    var ser1 = [7, 6, 9, 12]; // label:'NetComp'
-    var ser2 = [4, 5, 7, 19]; // label:'AnalyzerHR'
-    var ser3 = [5, 6, 7, 13]; // label:'Question Right'
-    
-    // time points as array
-    var weeks = ['week 50', 'week 51', 'week 52', 'week 53'];
-    
-    // bar chart generator
-    var plot2 = $.jqplot('plot2', [ser1, ser2, ser3], {
+    // ####################################
+    // #################bar chart generator
+    var plot2 = $.jqplot('plot2', [installations1, installations2, installations3], {
         seriesDefaults:{
             renderer:$.jqplot.BarRenderer, //all series as bars
             rendererOptions: {
                 barWidth: 8
             }
         },
+        // background and whole grid
+        grid: {
+            background: "white",
+            borderColor: "transparent", 
+            shadow: false
+        },
         // labels and colors for series
         series:[
-            {label:'NetComp', color: "red"},
-            {label:'AnalyzerHR', color: "blue"},
-            {label:'Question Right', color: "green"}
+            {label:labels[0], color: "#ed6e37", shadow: false},
+            {label:labels[1], color: "#15a0c8", shadow: false},
+            {label:labels[2], color: "#259e01", shadow: false}
         ],
         // legend options
         legend: {
             renderer: $.jqplot.EnhancedLegendRenderer,
             show: true,
-            placement: 'outsideGrid',
-            location:'s',
+            placement: "outsideGrid",
+            location: "s",
             rendererOptions: {
-              numberRows: '1',
-              numberColumns: '3'
+                numberRows: "1",
             },
         },
         // axes options
@@ -93,12 +109,13 @@ $(document).ready(function(){
                 ticks: weeks, // weeks array
                 tickOptions:{
                     showGridline: false, // no vertical lines
-                    textColor: '#05415b'
+                    textColor: "#05415b"
                 }
             },
             yaxis: {
+                min: 0,
                 tickOptions:{
-                    textColor: '#05415b'
+                    textColor: "#05415b",
                 }
             }
         }
@@ -106,24 +123,25 @@ $(document).ready(function(){
     // /bar chart
     
     // ###################################################
-    // ################################### sum for revenue
-    var sumRevenueArray = line1.concat(line2, line3); //merge arrays
-    var sumRevenue = 0;
-    for (var i=0; i < sumRevenueArray.length; i++){
-        for(var i2=1; i2 < sumRevenueArray[i].length; i2++){ //sum every second item in array - all integers
-            sumRevenue += sumRevenueArray[i][i2];
-        }
-    }
+    // ############################################### SUM
     
-    // ################################### sum for installations
-    var sumInstallationsArray = ser1.concat(ser2, ser3); ////merge arrays
-    var sumInstallations = 0;
-    for (var i=0; i < sumInstallationsArray.length; i++){ //sum all items
-        sumInstallations += sumInstallationsArray[i];
+    function sumValuesInAllSeries(nameOfSeries, numberOfSeries){
+        
+        // sam all values
+        var grandSum = 0;
+        for (var i=1; i <= numberOfSeries; i++){
+            for (var i2=0; i2 < window[nameOfSeries + i].length; i2++){
+                grandSum += window[nameOfSeries + i][i2];
+                
+                console.log(window[nameOfSeries + i][i2]);
+            }
+        }
+        
+        return grandSum;
     }
     
     // insert values to html file
-    $("#revenueSum").text(sumRevenue);
-    $("#installationsSum").text(sumInstallations);
+    $("#revenueSum").text(sumValuesInAllSeries("revenue", 3));
+    $("#installationsSum").text(sumValuesInAllSeries("installations", 3));
     
 });
